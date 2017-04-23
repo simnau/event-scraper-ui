6f64a81a723981ee025ee1eac8a5a0ac65ac341d
Simonas Naulickas <simonas.naulickas@gmail.com>
2017-04-23 08:57:32 +0300

    Redirect to index.html

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '/dist')));
+
+app.get('*', (req, res) => {
+  res.redirect('/index.html');
+});
 
 app.listen(process.env.PORT || 3000);
 
