d1054423b9890fd6603ae80575a10dd428c1f24f
Simonas Naulickas <simonas.naulickas@gmail.com>
2017-04-23 08:59:56 +0300

    remove redirect

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/dist')));
 
-app.get('*', (req, res) => {
-  res.redirect('/index.html');
-});
-
 app.listen(process.env.PORT || 3000);
 
 console.log('Server listening on ' + (process.env.PORT || 3000));
